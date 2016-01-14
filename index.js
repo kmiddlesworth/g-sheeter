ff0a5794755b6650c5f902c2ecc526a9b3b4ad18
kevin-middlesworth-designory-com <kevin.middlesworth@designory.com>
2016-01-14 13:45:09 -0600

    route from index.js

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-//node modules and global configs
-var express = require('express'),
-  app = express();
-
-
-
-app.get('*', function(req, res) {
-
-	console.log('ddd');
-
-});
-
-app.listen(3000, function() {
-
-    console.log('Running on port', 3000);
-
-});
+module.exports = require('./lib/constructor');
